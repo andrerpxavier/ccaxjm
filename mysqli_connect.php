3be83b019d2b1124ef71a17540426a2f79fd5111
André Xavier <57647584+andrerpxavier@users.noreply.github.com>
2025-06-13 00:17:09 +0100

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,8 +2,10 @@
 $db_error = null;
 $host = '10.0.2.4';
 $charset='utf8mb4';
+$dsn = "mysql:host=$host;dbname=telemoveis_bd;charset=$charset";
+
 try {
-    $pdo = new PDO("mysql:host="$host;"dbname=telemoveis_bd,charset="$charset, "root", "1234");
+    $pdo = new PDO($dsn, "root", "1234");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     $db_error = $e->getMessage();
